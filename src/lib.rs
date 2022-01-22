f6a3b5df4390049c5bd34fbae5ce71c508d7253b
Folling <coding@folling.de>
2022-01-22 02:39:31 +0100

    Setup lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+/*!
+graphene is an abstraction library over the various graphic APIs with a specific focus on 2D and application development.
+graphene is part of a larger application development framework called [alloy](https://github.com/Folling/alloy).
+
+It currently supports only OpenGL. However other graphic libraries will be added in the future.
+*/
+
+#![feature(register_tool)]
+#![register_tool(rust_analyzer)]
+// Enable clippy if our Cargo.toml file asked us to do so.
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+// Enable as many useful Rust and Clippy warnings as we can stand.  We'd
+// also enable `trivial_casts`, but we're waiting for
+// https://github.com/rust-lang/rust/issues/23416.
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![cfg_attr(feature = "clippy", warn(cast_possible_truncation))]
+#![cfg_attr(feature = "clippy", warn(cast_possible_wrap))]
+#![cfg_attr(feature = "clippy", warn(cast_precision_loss))]
+#![cfg_attr(feature = "clippy", warn(cast_sign_loss))]
+#![cfg_attr(feature = "clippy", warn(missing_docs_in_private_items))]
+#![cfg_attr(feature = "clippy", warn(mut_mut))]
+// Disallow `println!`. Use `debug!` for debug output
+// (which is provided by the `log` crate).
+#![cfg_attr(feature = "clippy", warn(print_stdout))]
+// This allows us to use `unwrap` on `Option` values (because doing makes
+// working with Regex matches much nicer) and when compiling in test mode
+// (because using it in tests is idiomatic).
+#![cfg_attr(all(not(test), feature = "clippy"), warn(result_unwrap_used))]
+#![cfg_attr(feature = "clippy", warn(unseparated_literal_suffix))]
+#![cfg_attr(feature = "clippy", warn(wrong_pub_self_convention))]
+
+/// Module for idiomatic OpenGL bindings. See the module's [Documentation](opengl)
+#[cfg(feature = "opengl")]
+pub mod opengl;
